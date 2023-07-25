be60b575694312e5979b9e87c6ef892d5f82f250
vanamayaswanth <vanamayaswanth@gmail.com>
2023-07-25 20:10:42 +0530

    updated langchain

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from app_util import classify
+from app_util import prompt_analysis
 
 app = Flask(__name__)
 
@@ -8,7 +8,7 @@ def index():
     if request.method == 'POST':
         openai_key = request.form['openai_key']
         prompt = request.form['prompt']
-        score, new_prompt = classify(prompt,openai_key)
+        score, new_prompt = prompt_analysis(prompt,openai_key)
         return render_template('result.html', score=score,new_prompt=new_prompt)
 
     return render_template('index.html')
