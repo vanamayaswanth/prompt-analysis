d403badaee719141a320542e4d77538564ad6eb5
vanamayaswanth <vanamayaswanth@gmail.com>
2023-07-27 13:11:24 +0530

    added enitity.py

--- app_util.py
+++ app_util.py
@@ -3,42 +3,43 @@ from langchain.prompts import PromptTemplate
 from langchain.llms import OpenAI
 from langchain import LLMChain
 from langchain.output_parsers import StructuredOutputParser, ResponseSchema
+from entity.prompt_entity import Prompt
 
 
-# llm = OpenAI(temperature=temp,model=model,max_token=max_token)
-
-
-def prompt_analysis(query,api_key,max_token,temp):
-    
-    llm = OpenAI(temperature=temp,max_tokens=max_token,openai_api_key=api_key)
-
-    prompt = """
+def prompt_analysis(query, api_key, temp, max_token):
+    llm = OpenAI(
+        temperature=temp,
+        max_tokens=max_token,
+        openai_api_key=api_key
+    )
+    prompt_template = """
     You need to analyze the given prompt {query} and provide a score on the scale of 1 to 100 based on the following parameters:. 
     The given prompt should be clear and specific, using complete sentences. The prompt might have  provide context.. 
     The prompt might have  system and user instructions .\n4. The prompt might  have explicit cues.. The prompt should be  broken down for  complex tasks 
     or limit the scope.Now, generate a new_prompt that satisfies all the parameters,{format_instructions}
-
     """
 
     response_schemas = [
-        ResponseSchema(name="score",description="this is the score for the given prompt"),
-        ResponseSchema(name="new_prompt",description="this is the new prompt for the given prompt")
+        ResponseSchema(name="score", description="this is the score for the given prompt"),
+        ResponseSchema(name="new_prompt", description="this is the new prompt for the given prompt")
     ]
     output_parser = StructuredOutputParser.from_response_schemas(response_schemas)
 
     format_instructions = output_parser.get_format_instructions()
 
-    example_prompt = PromptTemplate(input_variables=["query"],template=prompt,
-    partial_variables={"format_instructions": format_instructions})
+    example_prompt = PromptTemplate(
+        input_variables=["query"],
+        template=prompt_template,
+        partial_variables={"format_instructions": format_instructions}
+    )
 
-
-    _input = example_prompt.format_prompt(query=query)
+    prompt_entity = Prompt(query)
+    _input = example_prompt.format_prompt(query=prompt_entity.query)
     output = llm(_input.to_string())
 
-    x= output_parser.parse(output)
-    
-    score=x["score"]
-    
-    new_prompt=x["new_prompt"]
+    x = output_parser.parse(output)
 
-    return score,new_prompt
+    score = x["score"]
+    new_prompt = x["new_prompt"]
+
+    return score, new_prompt
---
+++ entity/prompt_entity.py
@@ -0,0 +1,3 @@
+class Prompt:
+    def __init__(self, query):
+        self.query = query
