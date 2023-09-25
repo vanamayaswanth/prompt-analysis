c0618316d871a5cded6292fb59e7f18402c1dbc6
Sethu Sai M <71321529+sethusaim@users.noreply.github.com>
2023-09-25 20:19:37 +0530

    Update app_util.py

--- app_util.py
+++ app_util.py
@@ -1,14 +1,14 @@
 from langchain.llms import OpenAI
 from langchain.output_parsers import ResponseSchema, StructuredOutputParser
 from langchain.prompts import PromptTemplate
-from langchain.chains import OpenAIModerationChain,SequentialChain,LLMChain
+from langchain.chains import OpenAIModerationChain
+
+def check_moderation(query,api_key):
+    moderation_chain = OpenAIModerationChain(openai_api_key=api_key)
+    moderation_chain.run(query)
 
 def prompt_analysis(query, api_key, temp, max_token):  # prompt_enity?
-    # llm = OpenAI(temperature=temp, max_tokens=max_token, openai_api_key=api_key)
-    # llm_chain = LLMChain(llm=llm)
-    # moderation_chain = OpenAIModerationChain(openai_api_key=api_key,)
-    # chain = SequentialChain(chains=[llm_chain, moderation_chain])
-    
+    llm = OpenAI(temperature=temp, max_tokens=max_token, openai_api_key=api_key)
     prompt_template = """
     Analyze the provided prompt {query} and assign a score from 1 to 100 using the following guidelines:
         - Ensure the given prompt is clear, specific, and presented in complete sentences.
@@ -18,6 +18,7 @@ def prompt_analysis(query, api_key, temp, max_token):  # prompt_enity?
         - If the task is complex, break down the prompt or set limitations to streamline the scope.
         Based on these criteria, craft a new_prompt that adheres to all the specified requirements, {format_instructions}
     """
+
     response_schemas = [
         ResponseSchema(
             name="score", description="this is the score for the given prompt"
@@ -35,33 +36,9 @@ def prompt_analysis(query, api_key, temp, max_token):  # prompt_enity?
         template=prompt_template,
         partial_variables={"format_instructions": format_instructions},
     )
-    
+
     _input = example_prompt.format_prompt(query=query)
-    llm = OpenAI(temperature=temp, max_tokens=max_token, openai_api_key=api_key)
-    llm_chain = LLMChain(llm=llm,prompt=example_prompt)
-    moderation_chain = OpenAIModerationChain(openai_api_key=api_key,)
-    chain = SequentialChain(chains=[llm_chain, moderation_chain])
-
-    # response_schemas = [
-    #     ResponseSchema(
-    #         name="score", description="this is the score for the given prompt"
-    #     ),
-    #     ResponseSchema(
-    #         name="new_prompt", description="this is the new prompt for the given prompt"
-    #     ),
-    # ]
-    # output_parser = StructuredOutputParser.from_response_schemas(response_schemas)
-
-    # format_instructions = output_parser.get_format_instructions()
-
-    # example_prompt = PromptTemplate(
-    #     input_variables=["query"],
-    #     template=prompt_template,
-    #     partial_variables={"format_instructions": format_instructions},
-    # )
-
-    # _input = example_prompt.format_prompt(query=query)
-    output = chain.run(_input.to_string())
+    output = llm(_input.to_string())
 
     x = output_parser.parse(output)
 
